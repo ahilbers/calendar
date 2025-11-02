b242e45cbea5fe17bbb446002e7e8a5a703d3ddb
Adriaan Hilbers <31656517+ahilbers@users.noreply.github.com>
2025-11-02 16:24:39 +0100

    add setup.py and .gitignore (#2)
    * update README, test github connection
    
    * add setup.py and .gitignore
    
    * add newline to .gitignore
    
    ---------
    
    Co-authored-by: ahilbers <ahilbers@Adriaans-MacBook-Air.fritz.box>
    Co-authored-by: ahilbers <ahilbers@41e9eb47-d56b-447b-be3b-81d57823e15c.fritz.box>

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='calendar',
+    version='0.0',
+    author='Adriaan Hilbers',
+    author_email='a.hilbers@icloud.com',
+    packages=[]
+)
