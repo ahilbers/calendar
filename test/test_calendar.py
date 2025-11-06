c2a371198413a37e2544e801436b0eafb3bd4c9a
ahilbers <a.hilbers@icloud.com>
2025-11-06 21:17:52 +0100

    use functions instead of fixtures in tests

--- test/test_calendar.py
+++ test/test_calendar.py
@@ -6,60 +6,61 @@ from schedules.calendar import Calendar
 from schedules.objects import Country, Day, Location, Person, StrID, Trip
 
 
-@pytest.fixture
 def sample_home_location() -> Location:
     return Location(country=Country.NETHERLANDS, city=StrID("Amsterdam"))
 
 
-@pytest.fixture
-def sample_person(sample_home_location: Location) -> Person:
+def sample_person() -> Person:
     return Person(
         last_name=StrID("lastname"),
         first_name=StrID("firstname"),
-        home=sample_home_location,
+        home=sample_home_location(),
     )
 
 
 class TestDailyCalendar:
     """Tests for calendar.get_daily_calendar."""
 
-    def test_no_trips(self, sample_person: Person):
+    def test_no_trips(self):
         """Without any trips, the daily calendar should be starting and ending each day in the home location."""
 
-        calendar = Calendar(person=sample_person)
+        person = sample_person()
+        calendar = Calendar(person)
 
         daily_calendar_actual = calendar.get_daily_calendar(dt.date(2024, 6, 22), dt.date(2024, 6, 24))
 
         daily_calendar_expected = {
-            dt.date(2024, 6, 22): Day(start=sample_person.home, end=sample_person.home),
-            dt.date(2024, 6, 23): Day(start=sample_person.home, end=sample_person.home),
-            dt.date(2024, 6, 24): Day(start=sample_person.home, end=sample_person.home),
+            dt.date(2024, 6, 22): Day(start=person.home, end=person.home),
+            dt.date(2024, 6, 23): Day(start=person.home, end=person.home),
+            dt.date(2024, 6, 24): Day(start=person.home, end=person.home),
         }
         assert daily_calendar_actual == daily_calendar_expected
 
-    def test_single_trip(self, sample_home_location: Location, sample_person: Person):
+    def test_single_trip(self):
         """A single trip inside the calendar should be calculated properly."""
 
+        person = sample_person()
         trip = Trip(
             location=Location(country=Country.NETHERLANDS, city=StrID("Zurich")),
             start_date=dt.date(2024, 6, 23),
             end_date=dt.date(2024, 6, 24),
         )
-        calendar = Calendar(person=sample_person)
+        calendar = Calendar(person)
         calendar.add_trip(trip)
 
         daily_calendar_actual = calendar.get_daily_calendar(dt.date(2024, 6, 22), dt.date(2024, 6, 25))
 
         daily_calendar_expected = {
-            dt.date(2024, 6, 22): Day(start=sample_home_location, end=sample_home_location),
-            dt.date(2024, 6, 23): Day(start=sample_home_location, end=trip.location),
-            dt.date(2024, 6, 24): Day(start=trip.location, end=sample_home_location),
-            dt.date(2024, 6, 25): Day(start=sample_home_location, end=sample_home_location),
+            dt.date(2024, 6, 22): Day(start=person.home, end=person.home),
+            dt.date(2024, 6, 23): Day(start=person.home, end=trip.location),
+            dt.date(2024, 6, 24): Day(start=trip.location, end=person.home),
+            dt.date(2024, 6, 25): Day(start=person.home, end=person.home),
         }
         assert daily_calendar_actual == daily_calendar_expected
 
-    def test_multiple_trips_disjoint(self, sample_home_location: Location, sample_person: Person):
+    def test_multiple_trips_disjoint(self):
 
+        person = sample_person()
         trip_1 = Trip(
             location=Location(country=Country.NETHERLANDS, city=StrID("Zurich")),
             start_date=dt.date(2024, 6, 23),
@@ -70,18 +71,18 @@ class TestDailyCalendar:
             start_date=dt.date(2024, 6, 25),
             end_date=dt.date(2024, 6, 26),
         )
-        calendar = Calendar(person=sample_person)
+        calendar = Calendar(person)
         calendar.add_trip(trip_1)
         calendar.add_trip(trip_2)
 
         daily_calendar_actual = calendar.get_daily_calendar(dt.date(2024, 6, 22), dt.date(2024, 6, 27))
 
         daily_calendar_expected = {
-            dt.date(2024, 6, 22): Day(start=sample_home_location, end=sample_home_location),
-            dt.date(2024, 6, 23): Day(start=sample_home_location, end=trip_1.location),
-            dt.date(2024, 6, 24): Day(start=trip_1.location, end=sample_home_location),
-            dt.date(2024, 6, 25): Day(start=sample_home_location, end=trip_2.location),
-            dt.date(2024, 6, 26): Day(start=trip_2.location, end=sample_home_location),
-            dt.date(2024, 6, 27): Day(start=sample_home_location, end=sample_home_location),
+            dt.date(2024, 6, 22): Day(start=person.home, end=person.home),
+            dt.date(2024, 6, 23): Day(start=person.home, end=trip_1.location),
+            dt.date(2024, 6, 24): Day(start=trip_1.location, end=person.home),
+            dt.date(2024, 6, 25): Day(start=person.home, end=trip_2.location),
+            dt.date(2024, 6, 26): Day(start=trip_2.location, end=person.home),
+            dt.date(2024, 6, 27): Day(start=person.home, end=person.home),
         }
         assert daily_calendar_actual == daily_calendar_expected
