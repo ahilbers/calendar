a64cd05058e5f7a714fe87a11b31791a10e34354
Adriaan Hilbers <31656517+ahilbers@users.noreply.github.com>
2025-11-09 19:34:44 +0100

    add home template (#19)
    * add home template
    
    * newline

--- schedules/frontend/pages.py
+++ schedules/frontend/pages.py
@@ -1,10 +1,10 @@
 """Define the pages of the website."""
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 pages = Blueprint("pages", __name__)
 
 
 @pages.route("/")
 def home() -> str:
-    return "<h1>Test</h1>"
+    return render_template("home.html")
