79ceae6ed9fa3d342fdb0f8652a949fe2ded2de3
Adriaan Hilbers <31656517+ahilbers@users.noreply.github.com>
2025-11-13 22:50:00 +0100

    create "add person" input form (#22)
    * create Add Person input form
    
    * add comment

--- schedules/frontend/pages.py
+++ schedules/frontend/pages.py
@@ -1,10 +1,13 @@
 """Define the pages of the website."""
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 pages = Blueprint("pages", __name__)
 
 
-@pages.route("/")
+@pages.route("/", methods=["GET", "POST"])
 def home() -> str:
+    if request.method == "POST":
+        data = dict(request.form)
+        print(data)
     return render_template("home.html")
