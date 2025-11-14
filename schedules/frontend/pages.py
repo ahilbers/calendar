11eac9e69fe7c4e3458890a09792edd5c83aec01
ahilbers <a.hilbers@icloud.com>
2025-11-14 22:27:17 +0100

    differentiate requests

--- schedules/frontend/pages.py
+++ schedules/frontend/pages.py
@@ -8,6 +8,8 @@ pages = Blueprint("pages", __name__)
 @pages.route("/", methods=["GET", "POST"])
 def home() -> str:
     if request.method == "POST":
-        data = dict(request.form)
-        print(data)
+        pass
+        # data = dict(request.form)
+        # if request.form.get("request_id") == "add_person":
+        #     print(f"Placeholder: add person {data}.")
     return render_template("home.html")
