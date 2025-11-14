3c291c51495e633d92ca691c43d06949143ac3fc
ahilbers <a.hilbers@icloud.com>
2025-11-14 22:28:54 +0100

    clean up

--- schedules/frontend/pages.py
+++ schedules/frontend/pages.py
@@ -8,8 +8,7 @@ pages = Blueprint("pages", __name__)
 @pages.route("/", methods=["GET", "POST"])
 def home() -> str:
     if request.method == "POST":
-        pass
-        # data = dict(request.form)
-        # if request.form.get("request_id") == "add_person":
-        #     print(f"Placeholder: add person {data}.")
+        data = dict(request.form)
+        if request.form.get("request_id") == "add_person":
+            _ = data  # Placeholder: add person to calendar
     return render_template("home.html")
