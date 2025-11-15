bbe7abe0a976118f7a6756bd6a3f3f054f35c26f
ahilbers <a.hilbers@icloud.com>
2025-11-15 15:01:42 +0100

    clean up

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 
 from schedules.frontend import create_app
 
-from schedules.frontend import requests
-
 if __name__ == "__main__":
-    # breakpoint()
     app = create_app()
     app.run(debug=True)
