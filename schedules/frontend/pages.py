67db1b9f63ce54e4cda770628aab55efc077948d
ahilbers <a.hilbers@icloud.com>
2025-12-04 21:22:01 +0100

    add new column for daily calendars

--- schedules/frontend/pages.py
+++ schedules/frontend/pages.py
@@ -15,6 +15,21 @@ def home() -> str:
     app = cast(AppWithCalendar, current_app)
     response = Response(code=200, message="Ready")
 
+    # TODO: DO NOT MERGE
+    person_1 = objects.Person(
+        last_name=objects.StrID("hilbers"),
+        first_name=objects.StrID("adriaan"),
+        home=objects.Location(objects.Country.SWITZERLAND, city=objects.StrID("zurich")),
+    )
+    person_2 = objects.Person(
+        last_name=objects.StrID("thorsdottir"),
+        first_name=objects.StrID("eva"),
+        home=objects.Location(objects.Country.NORWAY, city=objects.StrID("oslo")),
+    )
+    for person in [person_1, person_2]:
+        if person not in app.calendar.calendars.keys():
+            app.calendar._add_person(person)
+
     if flask_request.method == "POST":
         response = app.calendar.process_frontend_request(flask_request.form.to_dict())
     return render_template(
