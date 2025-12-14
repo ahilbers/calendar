a94192f8b8fbe65a498ac43c34ec45ec2957f74b
ahilbers <a.hilbers@icloud.com>
2025-12-14 14:51:08 +0100

    make website accessible over LAN

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from schedules.frontend import create_app
 if __name__ == "__main__":
     logger = logging.getLogger(__package__)
     app = create_app()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
