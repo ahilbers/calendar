c35ac0f9bc4f70f903462dd8ccb0cfbb7a7376e4
ahilbers <a.hilbers@icloud.com>
2026-01-18 20:46:46 +0100

    refactor calendar setup

--- schedules/frontend/app_with_calendar.py
+++ schedules/frontend/app_with_calendar.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from schedules.logic.calendar import FullCalendar
-from schedules.logic.storage import Base
+from schedules.logic.storage import Base, CalendarRepository
 
 
 class AppWithCalendar(Flask):
@@ -13,14 +13,17 @@ class AppWithCalendar(Flask):
 
     def __init__(self, import_name: str):
         super().__init__(import_name)
-        self.calendar = FullCalendar()
 
-        # Set up connection with database
-        database_engine = create_engine("sqlite:///data/database.db")
+        # Set up database
+        database_engine = create_engine("sqlite:///data/database.db")  # Created if it does not exist
         Base.metadata.create_all(database_engine)
-        self.database_session = sessionmaker(bind=database_engine)
+        self.database_session_maker = sessionmaker(bind=database_engine)
 
-        # Sync with current database state
-        session = self.database_session()
-        self.calendar.load_people_from_database(session)
+        # Create repository and calendar
+        session = self.database_session_maker()
+        database_repository = CalendarRepository(session)
+        self.calendar = FullCalendar(database_repository=database_repository)
+
+        # Load existing data
+        self.calendar.load_from_repository()
         session.close()
